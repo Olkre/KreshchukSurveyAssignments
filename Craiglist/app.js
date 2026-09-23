fcdcf43854ad01c2b5174d98ed57bdd0d5d2b3e1
Olkre <81689571+Olkre@users.noreply.github.com>
2026-09-22 21:20:58 -0400

    set up more initial data

--- Craiglist/app.js
+++ Craiglist/app.js
@@ -3,8 +3,23 @@ import bodyParser from 'koa-bodyparser';
 import { DatabaseSync } from 'node:sqlite';
 import routes from './routes.js';
 
-const db = new DatabaseSync('data.db');
-db.exec('CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY, text TEXT NOT NULL)');
+// initalize tables for categories and posts
+const db = new DatabaseSync('data.sqlite3');
+db.exec('CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY, name TEXT NOT NULL)');
+db.exec('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY, category_id INTEGER NOT NULL, title TEXT NOT NULL, body TEXT NOT NULL, createdts DATETIME NOT NULL)');
+
+// add initial data
+var categories = db.prepare('SELECT * FROM categories').all();
+
+if (categories.length === 0) {
+  db.exec(`INSERT INTO categories (name)
+    VALUES ('General'), ('Automotive'), ('Books')`);
+}
+
+// log initial data
+var categories = db.prepare('SELECT * FROM categories').all();
+console.log(categories);
+console.log("count" + categories.length);
 
 const router = routes(db);
 
--- Craiglist/routes.js
+++ Craiglist/routes.js
@@ -5,22 +5,33 @@ export default function routes(db) {
   const router = new Router();
 
   router.get('/', async (ctx) => {
-    const entries = db.prepare('SELECT * FROM entries').all();
-    ctx.body = await ejs.renderFile('views/index.ejs', { entries });
+    const categories = db.prepare('SELECT * FROM categories').all();
+    ctx.body = await ejs.renderFile('views/index.ejs', { categories });
   });
 
-  router.post('/entries', (ctx) => {
+  // insert a post
+  router.post('/posts', (ctx) => {
     const { text } = ctx.request.body;
     if (!text) ctx.throw(400, 'text is required');
-    const { lastInsertRowid } = db.prepare('INSERT INTO entries (text) VALUES (?)').run(text);
+    const { lastInsertRowid } = db.prepare('INSERT INTO posts (name) VALUES (?)').run(text);
     if (ctx.is('urlencoded')) return ctx.redirect('/');
     ctx.status = 201;
     ctx.body = { id: Number(lastInsertRowid), text };
   });
 
-  router.get('/entries', (ctx) => {
-    ctx.body = db.prepare('SELECT * FROM entries').all();
+  router.get('/categories', (ctx) => {
+    ctx.body = db.prepare('SELECT * FROM categories').all();
   });
+
+  router.get('/posts', (ctx) => {
+    ctx.body = db.prepare('SELECT * FROM posts').all();
+  });
+
+  router.get('/post/:id', (ctx) => {
+    const post = db.prepare('SELECT * FROM posts WHERE id = ?').get(ctx.params.id);
+    ctx.body = post;
+  });
+
 
   return router;
 }
