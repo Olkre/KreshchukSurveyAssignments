6ef1c72063ff7da30ecfeb236168cb05951e3328
Olkre <81689571+Olkre@users.noreply.github.com>
2026-09-22 22:08:11 -0400

    Align routes with requirements. fix db names

--- Craiglist/app.js
+++ Craiglist/app.js
@@ -3,10 +3,10 @@ import bodyParser from 'koa-bodyparser';
 import { DatabaseSync } from 'node:sqlite';
 import routes from './routes.js';
 
-// initalize tables for categories and posts
+// initalize tables for categories and postings
 const db = new DatabaseSync('data.sqlite3');
 db.exec('CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY, name TEXT NOT NULL)');
-db.exec('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY, category_id INTEGER NOT NULL, title TEXT NOT NULL, body TEXT NOT NULL, createdts DATETIME NOT NULL)');
+db.exec('CREATE TABLE IF NOT EXISTS postings (id INTEGER PRIMARY KEY, category_id INTEGER NOT NULL, title TEXT NOT NULL, body TEXT NOT NULL, createdts DATETIME NOT NULL)');
 
 // add initial data
 var categories = db.prepare('SELECT * FROM categories').all();
@@ -18,20 +18,20 @@ if (categories.length === 0) {
 
 // log initial data
 var categories = db.prepare('SELECT * FROM categories').all();
-console.log(categories);
-console.log("count" + categories.length);
+// console.log(categories);
+// console.log("count" + categories.length);
 
-// add some initials posts if empty
-var postings = db.prepare('SELECT * FROM posts').all();
-if (postings.length === 0) {
+// add some initials postings if empty
+var posts = db.prepare('SELECT * FROM posts').all();
+if (posts.length === 0) {
   db.exec(`INSERT INTO posts (category_id, title, body, createdts)
     VALUES (1, 'coffee machine', 'nice', CURRENT_TIMESTAMP),
     (2, 'jeep cherokee', '2014', CURRENT_TIMESTAMP),
     (3, 'thinking smart and slow', 'some guy', CURRENT_TIMESTAMP)
     `);
 }
-console.log(postings);
-console.log("count" + postings.length);
+// console.log(posts);
+// console.log("count" + posts.length);
 
 
 const router = routes(db);
--- Craiglist/routes.js
+++ Craiglist/routes.js
@@ -4,35 +4,53 @@ import ejs from 'ejs';
 export default function routes(db) {
   const router = new Router();
 
+  // GET / (shows a list of categories)
   router.get('/', async (ctx) => {
     const categories = db.prepare('SELECT * FROM categories').all();
     const postings = db.prepare('SELECT * FROM posts').all();
     ctx.body = await ejs.renderFile('views/index.ejs', { categories, postings });
   });
 
-  // insert a post
-  router.post('/postings', (ctx) => {
-    const { text } = ctx.request.body;
-    if (!text) ctx.throw(400, 'text is required');
-    const { lastInsertRowid } = db.prepare('INSERT INTO posts (name) VALUES (?)').run(text);
-    if (ctx.is('urlencoded')) return ctx.redirect('/');
-    ctx.status = 201;
-    ctx.body = { id: Number(lastInsertRowid), text };
+  // GET /posts [expected: category_id] (shows a list of titles)
+  router.get('/posts/:category_id', (ctx) => {
+    const { category_id } = ctx.params;
+
+    var posts = db.prepare('SELECT * FROM posts WHERE category_id = ?').all(category_id);
+    ctx.body = posts;
   });
 
-  router.get('/categories', (ctx) => {
-    ctx.body = db.prepare('SELECT * FROM categories').all();
+  // GET /post  [expected: post_id] (shows a single post)
+  router.get('/post/:post_id', (ctx) => {
+    const { post_id } = ctx.params;
+
+    var posts = db.prepare('SELECT * FROM posts WHERE id = ?').all(post_id);
+    ctx.body = posts;
   });
 
-  router.get('/postings', (ctx) => {
-    ctx.body = db.prepare('SELECT * FROM posts').all();
+  // GET /create [expected: category_id] (the form for adding a new post)
+  router.get('/create', async (ctx) => {
+    const categories = db.prepare('SELECT * FROM categories').all();
+    ctx.body = await ejs.renderFile('views/create.ejs', { categories });
   });
 
-  router.get('/posting/:id', (ctx) => {
-    const post = db.prepare('SELECT * FROM posts WHERE id = ?').get(ctx.params.id);
-    ctx.body = post;
-  });
+  // POST /create [expected: all the fields] (saves the post)
+  router.post('/create', (ctx) => {
+    const { title, body, category_id } = ctx.request.body;
+    console.log(title);
+    console.log(body);
+    console.log(category_id);
 
+    ctx.body = title + " " + body;
+
+    if (!category_id) ctx.throw(400, 'category is required');
+    if (!title) ctx.throw(400, 'title is required');
+    if (!body) ctx.throw(400, 'body is required');
+
+    //const { lastInsertRowid } = db.prepare('INSERT INTO posts (category_id, title, body, createdts) VALUES (?, ?, ?, CURRENT_TIMESTAMP)').run(title, body);
+    //if (ctx.is('urlencoded')) return ctx.redirect('/');
+    //ctx.status = 201;
+    //ctx.body = { id: Number(lastInsertRowid), text };
+  });
 
   return router;
 }
